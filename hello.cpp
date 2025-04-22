a050d2f625ae920fbe472adb93fd6121ff2bfd42
pipi0411 <20a41010217@students.hou.edu.vn>
2025-04-22 23:09:47 +0700

    .

--- hello.cpp
+++ hello.cpp
@@ -36,31 +36,58 @@ using namespace std;
 // }
 
 // Kiểu dữ liệu
+// int main() {
+//     int a = 10, b = 5;
+//     // Toán tử số học
+//     cout << "Tổng: " << a + b << endl;
+//     cout << "Hiệu: " << a - b << endl;
+//     cout << "Tích: " << a * b << endl;
+//     cout << "Thương: " << a / b << endl;
+//     cout << "Chia lấy dư: " << a % b << endl;
+
+//     // Toán tử so sánh
+//     cout << "So sánh: " << (a > b) << endl; // 1 là đúng, 0 là sai
+//     cout << "So sánh: " << (a < b) << endl; // 1 là đúng, 0 là sai
+//     cout << "So sánh: " << (a == b) << endl; // 1 là đúng, 0 là sai
+//     cout << "So sánh: " << (a != b) << endl; // 1 là đúng, 0 là sai
+//     cout << "So sánh: " << (a >= b) << endl; // 1 là đúng, 0 là sai
+
+//     // Toán tử logic
+//     cout << "Logic: " << (a > b && a < 20) << endl; // 1 là đúng, 0 là sai
+//     cout << "Logic: " << (a > b || a < 20) << endl; // 1 là đúng, 0 là sai
+//     cout << "Logic: " << !(a > b) << endl; // 1 là đúng, 0 là sai
+//     cout << "Logic: " << !(a < b) << endl; // 1 là đúng, 0 là sai
+//     cout << "Logic: " << !(a == b) << endl; // 1 là đúng, 0 là sai
+//     cout << "Logic: " << !(a != b) << endl; // 1 là đúng, 0 là sai
+
+//     cout << "Logic: " << !(a >= b) << endl; // 1 là đúng, 0 là sai
+//     cout << "Logic: " << !(a <= b) << endl; // 1 là đúng, 0 là sai
+//     return 0;
+// }
+
+// Câu điều kiện 
+// int main(){
+//     int number;
+//     cout << "Enter a number: ";
+//     cin >> number;
+
+//     if (number % 2 == 0){
+//         cout << "Số chẵn." << endl;
+//     }else {
+//         cout << "Số lẻ." << endl;
+//     }
+//     return 0;
+// }
+
+// Câu lệnh vòng lặp
 int main() {
-    int a = 10, b = 5;
-    // Toán tử số học
-    cout << "Tổng: " << a + b << endl;
-    cout << "Hiệu: " << a - b << endl;
-    cout << "Tích: " << a * b << endl;
-    cout << "Thương: " << a / b << endl;
-    cout << "Chia lấy dư: " << a % b << endl;
-
-    // Toán tử so sánh
-    cout << "So sánh: " << (a > b) << endl; // 1 là đúng, 0 là sai
-    cout << "So sánh: " << (a < b) << endl; // 1 là đúng, 0 là sai
-    cout << "So sánh: " << (a == b) << endl; // 1 là đúng, 0 là sai
-    cout << "So sánh: " << (a != b) << endl; // 1 là đúng, 0 là sai
-    cout << "So sánh: " << (a >= b) << endl; // 1 là đúng, 0 là sai
-
-    // Toán tử logic
-    cout << "Logic: " << (a > b && a < 20) << endl; // 1 là đúng, 0 là sai
-    cout << "Logic: " << (a > b || a < 20) << endl; // 1 là đúng, 0 là sai
-    cout << "Logic: " << !(a > b) << endl; // 1 là đúng, 0 là sai
-    cout << "Logic: " << !(a < b) << endl; // 1 là đúng, 0 là sai
-    cout << "Logic: " << !(a == b) << endl; // 1 là đúng, 0 là sai
-    cout << "Logic: " << !(a != b) << endl; // 1 là đúng, 0 là sai
-
-    cout << "Logic: " << !(a >= b) << endl; // 1 là đúng, 0 là sai
-    cout << "Logic: " << !(a <= b) << endl; // 1 là đúng, 0 là sai
+    for (int i = 0; i <= 10; i++){
+        for (int j = 1; j <= 10; j++){
+            // cout << i << " X " << j << " = " << i * j << endl;
+            // in bảng cửu chương
+            cout << i << " * " << j << " = " << i * j << "\t"; // ký tự đặc biệt dùng để tạo khoảng cách dạng tab (tab character).
+        }
+        cout << endl;
+    }
     return 0;
 }
